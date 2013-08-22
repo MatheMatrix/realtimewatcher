1364d4f00233879918c1e1d1d8e8f947e0f42007
MatheMatrix <mathematrix@foxmail.com>
2013-08-22 18:06:04 +0800

    PEP8 AccFrame.old.py

--- AccFrame.old.py
+++ AccFrame.old.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 import wx
 
@@ -12,17 +12,20 @@ import numpy as np
 import pylab
 import random
 
+
 class AccFrame(wx.Frame):
+
     '''docstring for AccFrame
     '''
 
-    def __init__(self, parent, id, stat):
+    def __init__(self, parent, id):  # , stat):
         '''Build Acc Frame
         '''
 
-        wx.Frame.__init__(self, parent, id, u'加速度实时监控', size = (1083, 720), style = wx.DEFAULT_FRAME_STYLE^(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
+        wx.Frame.__init__(self, parent, id, u'加速度实时监控', size=(1083, 720),
+                          style = wx.DEFAULT_FRAME_STYLE ^ (wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
 
-        self.ChangeStat = stat
+        #self.ChangeStat = stat
 
         self.panel = self.Display()
 
@@ -30,8 +33,9 @@ class AccFrame(wx.Frame):
         '''Display all elements
         '''
 
-        panel = wx.Panel(self, -1, pos = (0, 0), style = wx.NO_FULL_REPAINT_ON_RESIZE)
-        fig = Figure((9.0, 6.92), dpi = 100)
+        panel = wx.Panel(
+            self, -1, pos=(0, 0), style = wx.NO_FULL_REPAINT_ON_RESIZE)
+        fig = Figure((9.0, 6.92), dpi=100)
         rect = fig.patch
         rect.set_facecolor('#f0f0f0')
 
@@ -45,22 +49,26 @@ class AccFrame(wx.Frame):
         '''Display Title
         '''
 
-        title = wx.StaticText(panel, -1, u'加速度实时监控系统', (433, 50), (-1, -1), wx.ALIGN_CENTER)
+        title = wx.StaticText(
+            panel, -1, u'加速度实时监控系统', (433, 50), (-1, -1), wx.ALIGN_CENTER)
         title.SetForegroundColour('#0969A2')
-        font = wx.Font(18, wx.SWISS, wx.NORMAL, wx.BOLD, underline = False, faceName = 'Microsoft YaHei')
+        font = wx.Font(18, wx.SWISS, wx.NORMAL, wx.BOLD,
+                       underline=False, faceName='Microsoft YaHei')
         title.SetFont(font)
 
     def __del__(self):
-        self.ChangeStat('Acc')
+        #self.ChangeStat('Acc')
         for accplot in self.accplots:
             accplot.redraw_timer.Stop()
 
 
 class AccPlot():
+
     '''Paint a matplot curve and a combobox
     '''
 
-    chtypes = {1: ['ch1', 'ch2', 'ch3', 'ch4', 'ch5', 'ch6', 'ch7', 'ch8', 'ch9', 'ch10', 'ch11'], 2: ['ch12', 'ch13', 'ch14', 'ch15', 'ch16', 'ch17', 'ch18', 'ch19', 'ch20', 'ch21', 'ch22'], 3: ['ch22', 'ch23', 'ch24', 'ch25', 'ch26', 'ch27', 'ch28', 'ch29', 'ch30', 'ch31', 'ch32', 'ch33'], 4: ['ch32', 'ch33', 'ch34', 'ch35', 'ch36', 'ch37', 'ch38', 'ch39', 'ch40', 'ch41', 'ch42', 'ch43', 'ch44']}
+    chtypes = {1: ['ch1', 'ch2', 'ch3', 'ch4', 'ch5', 'ch6', 'ch7', 'ch8', 'ch9', 'ch10', 'ch11'], 2: ['ch12', 'ch13', 'ch14', 'ch15', 'ch16', 'ch17', 'ch18', 'ch19', 'ch20', 'ch21', 'ch22'], 3:
+               ['ch22', 'ch23', 'ch24', 'ch25', 'ch26', 'ch27', 'ch28', 'ch29', 'ch30', 'ch31', 'ch32', 'ch33'], 4: ['ch32', 'ch33', 'ch34', 'ch35', 'ch36', 'ch37', 'ch38', 'ch39', 'ch40', 'ch41', 'ch42', 'ch43', 'ch44']}
 
     def __init__(self, panel, posid, fig):
         '''init
@@ -76,20 +84,23 @@ class AccPlot():
 
         self.redraw_timer = wx.Timer()
         self.redraw_timer.Bind(wx.EVT_TIMER, self.on_redraw_timer)
-        self.redraw_timer.Start(100)
+        self.redraw_timer.Start(1000)
 
     def DisplayCombobox(self, panel, posid):
         '''Display combobox
         '''
 
-        chselect = wx.StaticText(panel, -1, u'请选择一个通道', (955 , 70 + (posid - 1) * 140), (-1, -1), wx.ALIGN_CENTER)
+        chselect = wx.StaticText(panel, -1, u'请选择一个通道', (
+            955, 70 + (posid - 1) * 140), (-1, -1), wx.ALIGN_CENTER)
         chselect.SetForegroundColour('#006D4C')
-        font = wx.Font(10, wx.SWISS, wx.NORMAL, wx.BOLD, underline = False, faceName = 'Microsoft YaHei')
+        font = wx.Font(10, wx.SWISS, wx.NORMAL, wx.BOLD,
+                       underline=False, faceName='Microsoft YaHei')
         chselect.SetFont(font)
 
-        cb = wx.ComboBox(panel, -1, self.chtypes[posid][0], (957, 90 + (posid - 1) * 140), 
-                         (80, -1), self.chtypes[posid],
-                         wx.CB_DROPDOWN)
+        cb = wx.ComboBox(
+            panel, -1, self.chtypes[posid][0], (957, 90 + (posid - 1) * 140),
+            (80, -1), self.chtypes[posid],
+            wx.CB_DROPDOWN)
 
         cb.Bind(wx.EVT_COMBOBOX, self.EvtComboBox)
 
@@ -114,22 +125,23 @@ class AccPlot():
 
         # self.fig = Figure((9.0, 6.92), dpi = 100)
         # rect = self.fig.patch
-        # # rect.set_facecolor('#f0f0f0')
+        # rect.set_facecolor('#f0f0f0')
 
-        self.axes = self.fig.add_subplot(411 + posid -1)
-        self.axes.set_position([0.03, 0.75 - (posid - 1) * (0.17 + 0.04 ), 0.95, 0.17])
+        self.axes = self.fig.add_subplot(411 + posid - 1)
+        self.axes.set_position(
+            [0.03, 0.75 - (posid - 1) * (0.17 + 0.04), 0.95, 0.17])
         self.axes.set_axis_bgcolor('white')
 
         self.axes.get_xaxis().set_visible(False)
         self.axes.get_yaxis().set_visible(False)
 
-        # plot the data as a line series, and save the reference 
+        # plot the data as a line series, and save the reference
         # to the plotted line series
         self.plot_data = self.axes.plot(
-            self.data, 
+            self.data,
             linewidth=1,
             color=(0, 0, 0),
-            )[0]
+        )[0]
 
     def drawplot(self):
         '''Draw plot
@@ -146,10 +158,10 @@ class AccPlot():
         # self.axes.grid(True, color='gray')
 
         # pylab.setp(self.axes.get_xticklabels(),  visible = True)
-        
+
         self.plot_data.set_xdata(np.arange(len(self.data)))
         self.plot_data.set_ydata(np.array(self.data))
-        
+
         self.canvas.draw()
 
     def on_redraw_timer(self, event):
@@ -157,14 +169,16 @@ class AccPlot():
         # (to respond to scale modifications, grid change, etc.)
         #
         self.data.append(self.datagen.next())
-        
+
         self.drawplot()
 
 
 class DataGen(object):
+
     """ A silly class that generates pseudo-random data for
         display in the plot.
     """
+
     def __init__(self, init=50):
         self.data = self.init = init
 
@@ -178,10 +192,16 @@ class DataGen(object):
 
         # if r > 0.9:
         #     self.data += delta * 15
-        # elif r > 0.8: 
-        #     # attraction to the initial value
+        # elif r > 0.8:
+        # attraction to the initial value
         #     delta += (0.5 if self.init > self.data else -0.5)
         #     self.data += delta
         # else:
         #     self.data += delta
         self.data = random.randint(10, 90)
+
+if __name__ == '__main__':
+    app = wx.PySimpleApp()
+    AccFrame(None, -1).Show()
+    app.MainLoop()
+
